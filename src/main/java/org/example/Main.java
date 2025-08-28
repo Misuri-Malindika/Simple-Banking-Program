8ddbef80abf0e439941962e54c3a3687b4149688
Misuri <misuri.malindika@gmail.com>
2025-08-28 16:12:58 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Declaring variables
+
+        Scanner scanner = new Scanner(System.in);
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
+        //Display Menu
+
+        //Get and Process User Choice
+
+        //showBalance()
+
+        //deposit()
+
+        //withdraw()
+
+        //Exit Message
+
+        Scanner.close();
+
+
+
+    }
+}
